883d24776d518a02e16d9551b68455af210318aa
kapils-repos <47316530+kapils-repos@users.noreply.github.com>
2019-02-14 14:38:28 +0530

    Delete merge.sh

--- merge.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-repository1="https://kapils-repos:Kgithub2019@github.com/kapils-repos/Developer-Repo-New.git"
-repository2="https://kapils-repos:Kgithub2019@github.com/kapils-repos/Config-Repo.git"
-localFolder1="C:/Git/developer_repo_new"
-localFolder2="C:/Git/config-repo"
-
-cd $localFolder2
-git remote add destination $repository2
-git push destination master
