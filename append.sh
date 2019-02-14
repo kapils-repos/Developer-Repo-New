44107c6a4ad23110d7aeb79173396bf7c4e32b3b
kapils-repos <47316530+kapils-repos@users.noreply.github.com>
2019-02-14 14:45:28 +0530

    Add files via upload

--- append.sh
+++ append.sh
@@ -6,10 +6,8 @@ localFolder2="C:/Git/config-repo"
 
 git clone $repository1 $localFolder1
 git clone $repository2 $localFolder2
-
-echo '\n\n--------------------------------------------------------------------\n' > $localFolder2/manifest.properties
-cat $localFolder1/manifest.txt >> $localFolder2/manifest.properties
-
+echo '\n\n--------------------------------------------------------------------\n' > C:/Git/config-repo/manifest.properties
+cat C:/Git/developer_repo_new/manifest.txt >> C:/Git/config-repo/manifest.properties
 cd $localFolder2
 git remote add destination $repository2
 git push destination master
