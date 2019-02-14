a1a9fe6e7341861bbc35fcca9f0b4078f7d0d295
kapils-repos <47316530+kapils-repos@users.noreply.github.com>
2019-02-14 14:38:04 +0530

    Delete clone.sh

--- clone.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-repository1="https://kapils-repos:Kgithub2019@github.com/kapils-repos/Developer-Repo-New.git"
-repository2="https://kapils-repos:Kgithub2019@github.com/kapils-repos/Config-Repo.git"
-localFolder1="C:/Git/developer_repo_new"
-localFolder2="C:/Git/config-repo"
-
-git clone $repository1 $localFolder1
-git clone $repository2 $localFolder2
