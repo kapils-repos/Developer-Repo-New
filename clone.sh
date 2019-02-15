c8cb0c2a90fcdda2d4ef12a56eca68feb80e3cec
kapils-repos <47316530+kapils-repos@users.noreply.github.com>
2019-02-15 10:46:27 +0530

    Add files via upload

--- clone.sh
+++ clone.sh
@@ -1,10 +1,8 @@
 #/bin/bash
 repository1="https://kapils-repos:Kgithub2019@github.com/kapils-repos/Developer-Repo-New.git"
 repository2="https://kapils-repos:Kgithub2019@github.com/kapils-repos/Config-Repo.git"
-localFolder1="C:/Git/developer_repo_new"
-localFolder2="C:/Git/config-repo"
+localFolder1="/home/travis/build/kapils-repos/Developer-Repo-New"
+localFolder2="/home/travis/build/kapils-repos/Config-Repo"
 
-git clone $repository1 $localFolder1
-git clone $repository2 $localFolder2
-cd $localFolder2
-pwd
+git clone $repository1
+git clone $repository2
--- merge.sh
+++ merge.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 repository1="https://kapils-repos:Kgithub2019@github.com/kapils-repos/Developer-Repo-New.git"
 repository2="https://kapils-repos:Kgithub2019@github.com/kapils-repos/Config-Repo.git"
-localFolder1="C:/Git/developer_repo_new"
-localFolder2="C:/Git/config-repo"
+localFolder1="/home/travis/build/kapils-repos/Developer-Repo-New"
+localFolder2="/home/travis/build/kapils-repos/Config-Repo"
 
 cd $localFolder2
 git remote add destination $repository2
