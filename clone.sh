ee4cdceac258eb6f766f9bc8d3c43d665a7a3d2a
kapils-repos <47316530+kapils-repos@users.noreply.github.com>
2019-02-15 12:33:53 +0530

    Add files via upload

--- clone.sh
+++ clone.sh
@@ -1,8 +1,8 @@
 #/bin/bash
-repository1="https://kapils-repos:Kgithub2019@github.com/kapils-repos/Developer-Repo-New.git"
-repository2="https://kapils-repos:Kgithub2019@github.com/kapils-repos/Config-Repo.git"
-localFolder1="/kapils-repos/Developer-Repo-New"
-localFolder2="/kapils-repos/Config-Repo"
 
-git clone --depth=50 --branch=master $repository2 kapils-repos/Config-Repo
+repository="https://kapils-repos:Kgithub2019@github.com/kapils-repos/Config-Repo.git"
+localFolder="Config-Repo"
+
+git clone --depth=50 --branch=master $repository Config-Repo
+cd Config-Repo
 ls
