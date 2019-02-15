dd6b908d77816c4d1eb099174eac9b5fb216c2a8
kapils-repos <47316530+kapils-repos@users.noreply.github.com>
2019-02-15 13:57:05 +0530

    Add files via upload

--- merge.sh
+++ merge.sh
@@ -2,4 +2,3 @@
 repository="https://kapils-repos:Kgithub2019@github.com/kapils-repos/Config-Repo.git"
 localFolder="/home/travis/build/kapils-repos/Developer-Repo-New/Config-Repo"
 cd Config-Repo
-git status
