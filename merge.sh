c97df77c2d1523af455db1a73a1614e4b478ded9
kapils-repos <47316530+kapils-repos@users.noreply.github.com>
2019-02-15 13:52:08 +0530

    Add files via upload

--- merge.sh
+++ merge.sh
@@ -2,9 +2,4 @@
 repository="https://kapils-repos:Kgithub2019@github.com/kapils-repos/Config-Repo.git"
 localFolder="/home/travis/build/kapils-repos/Developer-Repo-New/Config-Repo"
 
-cd "/home/travis/build/kapils-repos/Developer-Repo-New"
-#git status
-#git add "manifest.properties"
-#git commit -m "Updated the manifest.properties file"
-#git remote add destination $repository
-#git push destination master
+ls
