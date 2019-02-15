ac05e5211ca60eba49c3e81c160292fa1819ffe2
kapils-repos <47316530+kapils-repos@users.noreply.github.com>
2019-02-15 11:15:51 +0530

    Add files via upload

--- clone.sh
+++ clone.sh
@@ -1,14 +1,10 @@
 #/bin/bash
 repository1="https://kapils-repos:Kgithub2019@github.com/kapils-repos/Developer-Repo-New.git"
 repository2="https://kapils-repos:Kgithub2019@github.com/kapils-repos/Config-Repo.git"
-localFolder1="/home/travis/build/kapils-repos/Developer-Repo-New"
-localFolder2="/home/travis/build/kapils-repos/Config-Repo"
+localFolder1="/kapils-repos/Developer-Repo-New"
+localFolder2="/kapils-repos/Config-Repo"
 
 git clone $repository1 $localFolder1
-pwd
 git clone $repository2 $localFolder2
 cd $localFolder1
 pwd
-cd $localFolder2
-pwd
-ls
