3d902da3aa428dc14cd1dab631feb2007c49ec1f
kapils-repos <47316530+kapils-repos@users.noreply.github.com>
2019-02-15 12:54:54 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import sys
 
 os.system("sh clone.sh")
 
-file = open("/home/travis/build/kapils-repos/Developer-Repo-New/manifest.txt", 'r', encoding='utf-8')
-attributes = file.read()
+file1 = open("/home/travis/build/kapils-repos/Developer-Repo-New/manifest.txt", 'r', encoding='utf-8')
+attributes = file1.read()
 
 print(attributes)
 
@@ -12,5 +12,10 @@ file2 = open("/home/travis/build/kapils-repos/Developer-Repo-New/Config-Repo/man
 file2.write("\n\n---------------------------------------")
 file2.write("\n"+attributes)
 file2.close()
+
+file3 = open("/home/travis/build/kapils-repos/Developer-Repo-New/Config-Repo/manifest.properties",'r')
+output = file3.read()
+
+print(output)
 
 os.system("sh merge.sh")
