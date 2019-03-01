3e0c2a52057ff0a3385d7a0cb90541f93339d049
kapils-repos <47316530+kapils-repos@users.noreply.github.com>
2019-03-01 10:27:58 +0530

    Add files via upload

---
+++ attributes_read.py
@@ -0,0 +1,15 @@
+
+#file = open("C:/Users/kharindran/Desktop/sample.md", 'r', encoding='utf-8')
+
+#content = file.read()
+#text=content.split("---")
+
+#attributes = text[1].split("\n")
+
+#print(attributes[1])
+
+import os
+import sys
+
+out=os.system("sh file.sh")
+print(out)
