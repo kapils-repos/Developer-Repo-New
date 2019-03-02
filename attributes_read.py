e76b68c2ffebe5a0b04f551fe42f01bc360db3d4
kapils-repos <47316530+kapils-repos@users.noreply.github.com>
2019-03-02 20:28:03 +0530

    Add files via upload

--- attributes_read.py
+++ attributes_read.py
@@ -16,10 +16,11 @@ cmd = "git show --name-only --oneline"
 
 #output=os.system(cmd)
 output=subprocess.check_output(cmd, shell=True)
-print(output)
-list=str(output).split('\n')
+#print(output)
+val=str(output)
+list = val.split('\n')
+print(list)
 for x in list:
     if x.find('.md'):
         file=x
-
 print(file)
