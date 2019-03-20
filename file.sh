3d789fd2724af4a8003b92254a54c1963c9c4ed9
kapils-repos <47316530+kapils-repos@users.noreply.github.com>
2019-03-20 10:43:23 +0530

    Delete file.sh

--- file.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-repository="https://kapils-repos:Kgithub2019@github.com/kapils-repos/Developer-Repo-New.git"
-
-#git clone $repository "C:/Git/new-repo"
-cd Developer-Repo-New
-file=$(git show --name-only)
-echo $file
